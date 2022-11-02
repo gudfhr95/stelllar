fb0c07af0ce4bcbb7fb5799efa2ee5de1eaa7089
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-11-03 00:42:29 +0900

    feat: add Image

--- packages/server/src/post/entity/image.entity.ts
+++ packages/server/src/post/entity/image.entity.ts
@@ -1,6 +1,27 @@
-import { Embeddable } from '@mikro-orm/core';
-import { ObjectType } from '@nestjs/graphql';
+import { Embeddable, Property } from '@mikro-orm/core';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { GraphQLPositiveInt } from 'graphql-scalars';
 
 @Embeddable()
 @ObjectType()
-export class Image {}
+export class Image {
+  @Property({ columnType: 'text' })
+  @Field()
+  originalUrl: string;
+
+  @Property()
+  @Field(() => GraphQLPositiveInt)
+  originalWidth: number;
+
+  @Property()
+  @Field(() => GraphQLPositiveInt)
+  originalHeight: number;
+
+  @Property({ columnType: 'text', nullable: true })
+  @Field({ nullable: true })
+  smallUrl?: string;
+
+  @Property({ columnType: 'text', nullable: true })
+  @Field({ nullable: true })
+  popupUrl: string;
+}
