3ac1056740ba50fa8448e3aa32ae617fde274b91
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-11-03 10:10:05 +0900

    feat: add ChannelUser

--- packages/server/src/config/mikro-orm.config.ts
+++ packages/server/src/config/mikro-orm.config.ts
@@ -4,11 +4,12 @@ import { User } from '../user/entity/user.entity';
 import { Relationship } from '../user/entity/relationship.entity';
 import { Folder } from '../folder/entity/folder.entity';
 import { Reply } from '../comment/entity/reply.entity';
+import { ChannelUser } from '../channel/entity/channel-user.entity';
 
 const configService = new ConfigService();
 
 const MikroOrmConfig: Options = {
-  entities: [User, Relationship, Folder, Reply],
+  entities: [User, Relationship, Folder, Reply, ChannelUser],
   type: 'postgresql',
   host: configService.get('POSTGRES_HOST'),
   port: configService.get('POSTGRES_PORT'),
