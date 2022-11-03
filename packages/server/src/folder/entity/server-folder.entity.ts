c8a353d8fcb48a2e0356d27ba74cc2ebb5e1ed5e
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-11-03 09:41:07 +0900

    feat: add ServerFolder

--- packages/server/src/folder/entity/server-folder.entity.ts
+++ packages/server/src/folder/entity/server-folder.entity.ts
@@ -1,11 +1,23 @@
-import { Entity, ManyToOne, Property } from '@mikro-orm/core';
+import {
+  Entity,
+  ManyToOne,
+  OneToOne,
+  PrimaryKeyType,
+  Property,
+} from '@mikro-orm/core';
 import { Server } from '../../server/entity/server.entity';
 import { ReorderUtils } from '../../common/util/reorder-utils';
+import { Folder } from './folder.entity';
 
 @Entity()
 export class ServerFolder {
   @ManyToOne({ entity: () => Server, primary: true })
   server: Server;
+
+  @OneToOne({ entity: () => Folder, primary: true })
+  folder: Folder;
+
+  [PrimaryKeyType]: [string, string];
 
   @Property({ columnType: 'text' })
   position: string = ReorderUtils.FIRST_POSITION;
