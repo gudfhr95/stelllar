781def6050db177ec60f92789a85df1d60d90215
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-11-04 17:41:40 +0900

    chore: initialize turborepo

--- packages/server/src/server/entity/server-user-status.enum.ts
+++
@@ -1,9 +0,0 @@
-import { registerEnumType } from '@nestjs/graphql';
-
-export enum ServerUserStatus {
-  None = 'None',
-  Joined = 'Joined',
-  Banned = 'Banned',
-}
-
-registerEnumType(ServerUserStatus, { name: 'ServerUserStatus' });
