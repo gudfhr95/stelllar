be38ef9469595e0d296fd55fa7e521ea03797b9c
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-11-05 16:19:11 +0900

    chore: add tsconfig

---
+++ apps/server/src/app.controller.d.ts
@@ -0,0 +1,7 @@
+import { AppService } from "./app.service";
+export declare class AppController {
+    private readonly appService;
+    constructor(appService: AppService);
+    getHello(): string;
+}
+//# sourceMappingURL=app.controller.d.ts.map
---
+++ apps/server/src/app.service.d.ts
@@ -0,0 +1,4 @@
+export declare class AppService {
+    getHello(): string;
+}
+//# sourceMappingURL=app.service.d.ts.map
