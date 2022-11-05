73bc6c8e61b7b9da6e1ff85832e72b3599040d1e
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-11-05 16:35:53 +0900

    chore: add eslintrc

--- apps/server/src/app.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AppService } from "./app.service";
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getHello(): string;
-}
-//# sourceMappingURL=app.controller.d.ts.map
--- apps/server/src/app.service.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class AppService {
-    getHello(): string;
-}
-//# sourceMappingURL=app.service.d.ts.map
