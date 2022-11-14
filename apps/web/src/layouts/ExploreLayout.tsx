61a41bb7346fe9602726d4b768d3b4d6f9cbebed
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-11-14 23:40:00 +0900

    ui: add explore header

--- apps/web/src/layouts/ExploreLayout.tsx
+++ apps/web/src/layouts/ExploreLayout.tsx
@@ -1,15 +1,23 @@
+import { useTranslation } from "next-i18next";
 import { PropsWithChildren } from "react";
 import ExploreSidebar from "../components/explore/ExploreSidebar";
+import Header from "../components/ui/header/Header";
+import { IconExplore } from "../components/ui/icons/Icons";
 
 type ExploreLayout = {};
 
 export default function ExploreLayout({
   children,
 }: PropsWithChildren<ExploreLayout>) {
+  const { t } = useTranslation("explore");
+
   return (
-    <>
+    <div className="flex flex-grow">
       <ExploreSidebar />
-      {children}
-    </>
+      <div className="flex flex-col flex-grow">
+        <Header title={t("title")} icon={<IconExplore className="w-5 h-5" />} />
+        <div className="h-full">{children}</div>
+      </div>
+    </div>
   );
 }
