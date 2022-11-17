b955763c40d01c826a4e5ea52fc2e4bfb1d2f60f
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-11-17 15:34:44 +0900

    ui: add HomeHeader

--- apps/web/src/layouts/HomeLayout.tsx
+++ apps/web/src/layouts/HomeLayout.tsx
@@ -1,11 +1,15 @@
 import { PropsWithChildren } from "react";
+import HomeHeader from "../components/home/HomeHeader";
 import HomeSidebar from "../components/home/HomeSidebar";
 
 export default function HomeLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-grow">
       <HomeSidebar />
-      {children}
+      <div className="flex flex-col flex-grow">
+        <HomeHeader />
+        <div className="h-full">{children}</div>
+      </div>
     </div>
   );
 }
