3c4c9fce6d38e4fcf6040c283319222b83c17008
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-11-23 14:20:25 +0900

    feature(seo): add default seo

---
+++ apps/web/next-seo.config.js
@@ -0,0 +1,21 @@
+export const defaultSeoConfig = {
+  title: "Stelllar",
+  description:
+    "Stellar는 오픈 소스 기반의 익명 커뮤니티를 위한 게시판, 채팅 플랫폼입니다.",
+  canonical: "https://stelllar.co",
+  openGraph: {
+    type: "website",
+    locale: "ko",
+    url: "https://stelllar.co",
+    title: "Stelllar",
+    site_name: "Stelllar",
+    images: [
+      {
+        url: "https://repository-images.githubusercontent.com/560377670/bc508f61-d4f7-4b67-bd1e-c1115265252a",
+        width: 1280,
+        height: 640,
+        alt: "Stelllar",
+      },
+    ],
+  },
+};
--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { ApolloProvider } from "@apollo/client";
 import { SessionProvider } from "next-auth/react";
 import { appWithTranslation } from "next-i18next";
+import { DefaultSeo } from "next-seo";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import apolloClient from "../apollo-client";
+import { defaultSeoConfig } from "../next-seo.config";
 import ContextMenuProvider from "../src/components/ui/context/ContextMenuProvider";
 import { usePreviousPath } from "../src/hooks/usePreviousPath";
 import MainLayout from "../src/layouts/MainLayout";
@@ -16,11 +18,12 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 
   return (
     <>
+      <DefaultSeo {...defaultSeoConfig} />
       <Head>
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/svg+xml" href="/icons/icon.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Stelllar – Forum & Chat for Communities</title>
+        <title>Forum & Chat for Communities - Stelllar</title>
       </Head>
       <main className="app">
         <ApolloProvider client={apolloClient}>
