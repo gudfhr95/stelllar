dafbeb010fafa3fb67827220b966b36ba2cb4142
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-11-23 15:08:59 +0900

    feature(seo): add Google Analytics

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -4,11 +4,13 @@ import { appWithTranslation } from "next-i18next";
 import { DefaultSeo } from "next-seo";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import apolloClient from "../apollo-client";
 import { defaultSeoConfig } from "../next-seo.config";
 import ContextMenuProvider from "../src/components/ui/context/ContextMenuProvider";
 import { usePreviousPath } from "../src/hooks/usePreviousPath";
 import MainLayout from "../src/layouts/MainLayout";
+import * as gtag from "../src/lib/gtag";
 import "../src/styles/global.css";
 import "../src/styles/index.css";
 import "../src/styles/tippy.css";
@@ -25,6 +27,27 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Forum & Chat for Communities - Stelllar</title>
       </Head>
+
+      {/* Google tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${gtag.GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+        }}
+      />
+
       <main className="app">
         <ApolloProvider client={apolloClient}>
           <SessionProvider session={session}>
